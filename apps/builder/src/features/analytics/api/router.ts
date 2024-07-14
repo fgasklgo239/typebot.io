9f9f6ad647f7d7affb9900a7fa021cbc6f182c11
ASUIHYODG9B78IG2C3R8OUY <sergeikraven@proton.me>
2024-07-14 17:19:34 -0300

    remove

--- apps/builder/src/features/analytics/api/router.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '@/helpers/server/trpc'
-import { getStats } from './getStats'
-import { getInDepthAnalyticsData } from './getInDepthAnalyticsData'
-
-export const analyticsRouter = router({
-  getInDepthAnalyticsData,
-  getStats,
-})
--- apps/builder/src/features/forge/ForgedBlockLabel.tsx
+++
@@ -1,9 +0,0 @@
-import { ForgedBlock } from '@typebot.io/forge-repository/types'
-import { useForgedBlock } from './hooks/useForgedBlock'
-import { Text } from '@chakra-ui/react'
-
-export const ForgedBlockLabel = ({ type }: { type: ForgedBlock['type'] }) => {
-  const { blockDef } = useForgedBlock(type)
-
-  return <Text fontSize="sm">{blockDef?.name}</Text>
-}
